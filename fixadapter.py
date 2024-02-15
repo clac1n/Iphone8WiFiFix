836a7b11943af50d669963053fd0ac9977dff0ef
c1ack <spaksgm@gmail.com>
2024-02-15 16:20:07 +0300

    v1

--- fixadapter.py
+++ fixadapter.py
@@ -1,32 +1,23 @@
 import vlc
 import time
 
-def play_radio_station(url):
+url = 'http://inhold.org:8000/primvolna-nhk'
+
+instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+
+player = instance.media_player_new()
+
+media = instance.media_new(url)
+
+player.set_media(media)
+
+player.audio_toggle_mute()
+
+player.play()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
     
-    instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
-
-   
-    player = instance.media_player_new()
-
-   
-    media = instance.media_new(url)
-
-   
-    player.set_media(media)
-
-   
-    player.play()
-
-  
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        
-        player.stop()
-
-
-radio_url = 'http://inhold.org:8000/primvolna-nhk'
-play_radio_station(radio_url)
-
-
+    player.stop()
